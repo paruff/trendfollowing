71348986c602f9cdf1cd74f511b879b5f6701344
Phil Ruff <phil.ruff@scaledmarkets.com>
2017-08-14 10:48:37 -0400

    init from spring start

---
+++ src/main/java/org/llap/trendfollowing/notice/NoticeApplication.java
@@ -0,0 +1,12 @@
+package org.llap.trendfollowing.notice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeApplication.class, args);
+	}
+}
